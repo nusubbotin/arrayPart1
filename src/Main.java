c92651aa37e0996723ceb531117e9bd6c39b80ad
nusubbotin <nu.subbotin@ya.ru>
2022-09-28 13:01:51 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
 
+    // Задание 1
+    public static void task1(){
+        System.out.println("Задание 1:");
+
+        int [] box = new int[]{1, 2, 3};
+        double [] num = {1.57, 7.654, 9.986};
+        char [] anyArr = new char[2];
+
+    }
+
+    public static void main(String[] args) {
+        task1();
     }
 }
