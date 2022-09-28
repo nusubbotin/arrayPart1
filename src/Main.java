2027736f351160d594bd7024fb6899d0e3bba472
nusubbotin <nu.subbotin@ya.ru>
2022-09-28 13:10:23 +0300

    2 задача

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,45 @@ public class Main {
 
     }
 
+    // Задание 2
+    public static void task2(){
+        System.out.println("Задание 2:");
+
+        int [] box = new int[]{1, 2, 3};
+        double [] num = {1.57, 7.654, 9.986};
+        char [] anyArr = {'a', 'b'};
+
+        for (int i = 0; i < box.length; i++) {
+            System.out.print(box[i]);
+            if (i < box.length-1) {
+                System.out.print(", ");
+            }else {
+                System.out.print("\n");
+            }
+        }
+
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i]);
+            if (i < num.length-1) {
+                System.out.print(", ");
+            }else {
+                System.out.print("\n");
+            }
+        }
+
+        for (int i = 0; i < anyArr.length; i++) {
+            System.out.print(anyArr[i]);
+            if (i < anyArr.length-1) {
+                System.out.print(", ");
+            }else {
+                System.out.print("\n");
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         task1();
+        task2();
     }
 }
